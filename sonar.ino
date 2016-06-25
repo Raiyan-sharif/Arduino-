9cc5569a7d0ef79ba58c86456d1bc2d6075e0399
Raiyan <Raiyan-sharif@users.noreply.github.com>
2016-06-25 13:40:14 +0600

    Add files via upload

---
+++ sonar.ino
@@ -0,0 +1,22 @@
+#define ECHOPIN 10
+#define TRIGPIN 9
+void setup() {
+  Serial.begin(9600);
+  pinMode(ECHOPIN, INPUT);
+  pinMode(TRIGPIN, OUTPUT);
+} m
+
+void loop() {
+  digitalWrite(TRIGPIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW);
+
+  //Distance Calculation
+  float distance = pulseIn(ECHOPIN, HIGH);
+  distance = distance+10;
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+}
